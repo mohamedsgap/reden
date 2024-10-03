1c7a5cfaaf7552ee53cd8fdcfa675debe7eed0d1
Mohamed Abdelnasser <mmabdelnasser@gmail.com>
2024-10-03 19:14:57 +0300

    feat: implement db query functions

---
+++ src/db/queries/posts.ts
@@ -0,0 +1,51 @@
+import type { Post } from "@prisma/client";
+import { db } from "@/db";
+
+export type _PostWithData = Post & {
+  topic: { slug: string };
+  user: { name: string | null };
+  _count: { comments: number };
+};
+export type PostWithData = Awaited<
+  ReturnType<typeof fetchPostsByTopicSlug>
+>[number];
+export function fetchPostsByTopicSlug(slug: string) {
+  return db.post.findMany({
+    where: { topic: { slug } },
+    include: {
+      topic: { select: { slug: true } },
+      user: { select: { name: true } },
+      _count: { select: { comments: true } },
+    },
+  });
+}
+
+export function fetchTopPosts(): Promise<_PostWithData[]> {
+  return db.post.findMany({
+    orderBy: [
+      {
+        comments: {
+          _count: "desc",
+        },
+      },
+    ],
+    include: {
+      topic: { select: { slug: true } },
+      user: { select: { name: true, image: true } },
+      _count: { select: { comments: true } },
+    },
+  });
+}
+
+export function fetchPostsBySearchTerm(term: string): Promise<PostWithData[]> {
+  return db.post.findMany({
+    include: {
+      topic: { select: { slug: true } },
+      user: { select: { name: true, image: true } },
+      _count: { select: { comments: true } },
+    },
+    where: {
+      OR: [{ title: { contains: term } }, { content: { contains: term } }],
+    },
+  });
+}
