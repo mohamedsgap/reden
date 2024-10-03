29f854f13da58856f9137932892bc7cda8fa8275
Mohamed Abdelnasser <mmabdelnasser@gmail.com>
2024-10-03 19:27:49 +0300

    feat: creat all actions

---
+++ src/actions/create-post.ts
@@ -0,0 +1,83 @@
+"use server";
+import { z } from "zod";
+import { auth } from "@/auth";
+import type { Post } from "@prisma/client";
+import { redirect } from "next/navigation";
+import { db } from "@/db";
+import paths from "@/paths";
+import { revalidatePath } from "next/cache";
+const createPostSchema = z.object({
+  title: z.string().min(3),
+  content: z.string().min(10),
+});
+
+interface CreatePostFormState {
+  errors: {
+    title?: string[];
+    content?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createPost(
+  slug: string,
+  fromState: CreatePostFormState,
+  formData: FormData
+): Promise<CreatePostFormState> {
+  const result = createPostSchema.safeParse({
+    title: formData.get("title"),
+    content: formData.get("content"),
+  });
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+  const session = await auth();
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ["You must be signed in to do this"],
+      },
+    };
+  }
+
+  const topic = await db.topic.findFirst({
+    where: { slug },
+  });
+  if (!topic) {
+    return {
+      errors: {
+        _form: ["Can not find the topic"],
+      },
+    };
+  }
+  let post: Post;
+  try {
+    post = await db.post.create({
+      data: {
+        title: result.data.title,
+        content: result.data.content,
+        userId: session.user.id,
+        topicId: topic.id,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        errors: {
+          _form: [err.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          _form: ["something went wrong"],
+        },
+      };
+    }
+  }
+
+  revalidatePath(paths.topicShow(slug));
+  redirect(paths.postShow(slug, post.id));
+}
--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,10 +1,6 @@
-'use server'
-import * as auth from '@/auth';
-
-export async function singIn() {
-    return auth.signIn('github')
-}
-
-export async function signOut() {
-    return auth.signOut()
-}
+export { createComment } from "./create-comment";
+export { createPost } from "./create-post";
+export { createTopic } from "./create-topic";
+export { signIn } from "./sign-in";
+export { signOut } from "./sign-out";
+export { search } from "./search";
